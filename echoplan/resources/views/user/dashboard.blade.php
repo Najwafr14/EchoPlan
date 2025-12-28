3790c7a73787449a17b5bbf28d52fa0779be14f9
Najwafr14 <79633048+Najwafr14@users.noreply.github.com>
2025-12-29 05:18:27 +0700

    Delete echoplan directory

--- echoplan/resources/views/user/dashboard.blade.php
+++
@@ -1,9 +0,0 @@
-<x-app-layout>
-    <div class="header">
-        <a id="sidebarToggle" class="sidebar-toggle">
-            ☰
-        </a>
-        <p>Dashboard - {{ auth()->user()->role }}</p>
-        <b>{{ auth()->user()->full_name }}</b>
-    </div>
-</x-app-layout>
